14f49a767f8aabf34b8f650dc6273302b9563b6c
azartechnet <azartechnet@gmail.com>
2024-07-29 18:50:35 +0530

    updated

--- Reactjs/evemern/src/index.js
+++ Reactjs/evemern/src/index.js
@@ -81,7 +81,7 @@ const r1=ReactDOM.createRoot(document.getElementById('root'));
 r1.render(<Myelem/>);*/
 
 //Fragement Tag
-const App=()=>{
+/*const App=()=>{
     return(
        
         <>
@@ -91,8 +91,177 @@ const App=()=>{
     )        
 }
 const r1=ReactDOM.createRoot(document.getElementById('root'));
-r1.render(<App/>);
+r1.render(<App/>);*/
 
+//Importing file and Bootstrap
 
+/*import './index.css';
+
+const App=()=>{
+    return(
+        <>
+          <h1>Welcome</h1>
+          <form>
+            <div className="mb-3">
+                <label className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                <div id="emailHelp" className="form-text">We'll never share your email with
+                anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"></input>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+        </>
+    )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+//Conditional Rendering
+
+/*const x=11
+let text="hello";
+if(x>10){
+    text="world"
+    }else{
+        text="hello"
+        }
+        console.log(text)
+        //Output: hello
+    
+const Myelem=()=>{
+   
+    return(
+        <div>
+            {text}Welcome!!
+        </div>
+    )
+
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Myelem/>)*/
+
+//class component
+
+/*class Sample extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)*/
+
+//else if ladder
+
+/*const Gradesystem=()=>{
+
+    let grade;
+    const studentmark=prompt("Enter the your marks??")
+
+    if(studentmark>=90)
+    {
+        grade='A';
+    }
+    else if(studentmark>=70)
+    {
+        grade='B';
+    }
+    else if(studentmark>=60)
+    {
+        grade='C';
+    }
+    else
+    {
+        grade='F';
+    }
+    return(
+        <div>
+            <h1>Your Grade is::{grade}</h1>
+        </div>
+    )
+
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Gradesystem/>)*/
+
+//Switch Statement
+
+/*const getGrade=(score)=>{
+
+    switch(true)
+    {
+        case score>=90:
+            return 'A';
+        case score>=80:
+            return 'B';
+        case score>=70:
+            return 'C';
+        default:
+            return 'F';
+    }
+}
+
+const GradeComponent=()=>{
+
+    const score=90;
+    const grade=getGrade(score);
+
+    return(
+        <div>
+            <h1>Your Grade is::{grade}</h1>
+            </div>
+    )
+    
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<GradeComponent/>)*/
+
+//Function arguments with props
+
+/*function Sample(props)
+{
+    return(
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.age}</h2>
+        </div>
+    )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample name="John" age="25"/>)*/
+
+//Component in Component
+
+function Sample()
+{
+    return(
+        <h1>This is First Component</h1>
+    )
+}
+function Sample1()
+{
+    return(
+        <div>
+            This is Second Component
+            <Sample/>
+        </div>
+    )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample1/>)
 
 
