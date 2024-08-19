7abf0e6400c8cccafa3efabab5a32104468d4994
azartechnet <azartechnet@gmail.com>
2024-08-19 18:47:28 +0530

    updated

--- Reactjs/routerapp/src/pages/Home.js
+++ Reactjs/routerapp/src/pages/Home.js
@@ -1,5 +1,49 @@
-const Home=()=>
-{
-    return<h1>Home Page</h1>
-}
+import c1 from './images/c1.jpeg';
+import c2 from './images/c2.jpeg';
+import c3 from './images/c3.jpeg';
+import './flex.css';
+const Home = () => {
+  return (
+    <>
+      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={c1} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={c2} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={c3} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div className="flex">
+        <div className="box">Box 1</div>
+        <div className="box">Box 2</div>
+        <div className="box">Box 3</div>
+      </div>
+      <div className="flex">
+        <div className="box">Box 1</div>
+        <div className="box">Box 2</div>
+        <div className="box">Box 3</div>
+      </div>
+      <div className="flex">
+        <div className="box">Box 1</div>
+        <div className="box">Box 2</div>
+        <div className="box">Box 3</div>
+      </div>
+    </>
+  );
+};
+
 export default Home;
